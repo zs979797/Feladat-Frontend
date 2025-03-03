8ef09d9c65a430afdb5a6d3b481eb9b17cb5be52
Holovics Zsombor <Zsombor@bp20pmh.local>
2025-03-03 15:17:35 +0100

    Frontend initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import TaskCalendar from "./TaskCalendar";
+
+function App() {
+  return (
+    <div>
+      <h1>Feladatkezelő Rendszer</h1>
+      <TaskCalendar />
+    </div>
+  );
+}
+
+export default App;
